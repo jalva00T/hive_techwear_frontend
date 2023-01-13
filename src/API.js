f26382e829355f8577e333a53367911981fbf21f
Jake Mercedes Alvarez <76409353+jalva00T@users.noreply.github.com>
2023-01-13 15:11:05 -0600

    Changed BASE_URL in API.js from Heroku backend to Replit backend.

--- src/API.js
+++ src/API.js
@@ -6,7 +6,7 @@ var baseURL;
 if (process.env.REACT_APP_ENVIRONMENT && process.env.REACT_APP_ENVIRONMENT === "PRODUCTION") {
 	baseURL = process.env.REACT_APP_API_BASE_URL;
 } else {
-	baseURL = "https://hive-techware-backend.herokuapp.com/";
+	baseURL = "https://hivetechwearbackend.jmatechis.repl.co";
 }
 
 const api = axios.create({
